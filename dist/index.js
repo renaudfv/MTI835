f4bc0d6fc9cf5cd02caeffebce2fdbb5221b17e4
Renaud Vincent <renaud.vincent.1@ens.etsmtl.ca>
2016-06-02 18:33:24 -0400

    Soundcloud setup, having CORS error for request origin + 2 method setup for scene rendering

--- dist/index.js
+++ dist/index.js
@@ -41747,40 +41747,61 @@ THREE.MorphBlendMesh.prototype.update = function ( delta ) {
 },{}],2:[function(require,module,exports){
 var THREE = require('three');
 
-var scene, camera, renderer;
+var scene, camera;
 var geometry, material, mesh;
 
-init();
-animate();
+var renderer = new THREE.WebGLRenderer();
+trackPlayer = undefined;
 
-function init() {
+function renderHome() {
 
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.z = 1000;
 
-    geometry = new THREE.BoxGeometry( 500, 500, 500 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    geometry = new THREE.BoxGeometry( 500, 500, 1 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    document.body.appendChild( renderer.domElement );
-
-}
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    // renderer = new THREE.WebGLRenderer();
+    // renderer.setSize( window.innerWidth, window.innerHeight );
+    // Render once
+    // if( !contextRendered ) { 
+    //     document.body.appendChild( renderer.domElement ); 
+    // }
 
     renderer.render( scene, camera );
+} 
+function renderTrack() {
+
+    // Track scene rendering
 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Initialise Soundcloud API
+    SC.initialize({
+        client_id: 'c1da0911d3af90cfd3153d5c6d030137'
+    });
+
+    //Renderer setup, should be valid for the whole scope context
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement ); 
+
+    SC.get('/tracks', {
+        q: 'erased tapes'
+    }).then(function(tracks){
+        console.log(tracks);
+        // SC.stream(tracks[0].uri).then(function(player){
+        //     trackPlayer = player;
+        // });
+        renderHome();
+    });
+
+    // renderHome();
+});
 },{"three":1}]},{},[2]);
