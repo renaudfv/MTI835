b66459578ce81066f7a6a818cb0a5f2f93b65cd3
Renaud Vincent <renaudvincent@Renauds-MacBook-Pro.local>
2016-06-21 14:38:15 -0400

    Anti-alias, resize and mouse navigation

--- src/index.js
+++ src/index.js
@@ -1,36 +1,119 @@
 var THREE = require('three');
+var _ = require('underscore');
 
-var scene, camera;
-var geometry, material, mesh;
+var PI = Math.PI;
 
-var renderer = new THREE.WebGLRenderer();
+var scene, camera, ambientLight;
+
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
 trackPlayer = undefined;
 
-function renderHome() {
+// Renders and update with browser refresh rate
+function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+}
+
+function renderHome(tracks) {
 
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    // camera.position.z = 0;
+    camera.rotation.Z = Math.PI/6;
+    
+    var tracksGroup = new THREE.Object3D();
+    scene.add( tracksGroup );
 
-    geometry = new THREE.BoxGeometry( 500, 500, 1 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    // ambientLight = new THREE.AmbientLight(0xbbbbbb);
+    // scene.add(ambientLight);
 
-    // renderer = new THREE.WebGLRenderer();
-    // renderer.setSize( window.innerWidth, window.innerHeight );
-    // Render once
-    // if( !contextRendered ) { 
-    //     document.body.appendChild( renderer.domElement ); 
-    // }
+    // geometry = new THREE.BoxGeometry( 500, 500, 1 );
+    // material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 
-    renderer.render( scene, camera );
+    // mesh = new THREE.Mesh( geometry, material );
+    // scene.add( mesh );
+
+    var imageUrl = '';
+    var geometry, mesh, material, texture;
+
+    var loader = new THREE.TextureLoader();
+
+    _.each(tracks, function(track) {
+
+        // Load track image or avatar url if none
+        imageUrl = ( !!track.artwork_url ) ? track.artwork_url : track.user.avatar_url;
+        texture = loader.load( imageUrl );
+        console.log(texture)
+
+        material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+        geometry = new THREE.PlaneGeometry( 500, 500 );
+        mesh = new THREE.Mesh( geometry, material );
+
+        tracksGroup.add( mesh )
+        
+        // loader.load( imageUrl, function ( texture ) {
+        //     console.log(texture)
+        //     // do something with the texture
+        //     material = new THREE.MeshBasicMaterial( {
+        //         map: texture
+        //     } );
+
+        //     geometry = new THREE.PlaneGeometry( 500, 500 );
+        //     mesh = new THREE.Mesh( geometry, material );
+
+        //     tracksGroup.add( mesh );
+
+        // }, function ( xhr ) {
+        //     //Loading
+        //     console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+
+        // }, function ( xhr ) {
+        //     // Error
+        //     console.log( 'An error happened' );
+
+        // } );
+
+    });
+
+    tracksGroup.translateZ(-3000);
+    tracksGroup.lookAt(camera.position);
+    render();
 } 
+
 function renderTrack() {
 
     // Track scene rendering
+
+}
+
+window.addEventListener( 'resize', windowResize);
+
+function windowResize(){
+    //set aspect ratio
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    //set render size
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+window.addEventListener("mousemove", rotateCamera); 
+
+var mouseScreenRatioY = 0, mouseScreenRatioX = 0;
+
+function rotateCamera(event) {
+
+    // Convert mouse X position between -PI/3 and PI/3 for horizontal rotation (120˚ FOV) 
+    // has been optimized/factorized from prevision, formula might seem odd
+    mouseScreenRatioY = 2 * event.clientX / window.innerWidth - 1; 
+    camera.rotation.y = - mouseScreenRatioY * PI / 3;
+    
+    // Convert mouse Y position between -PI/2 and PI/2 for vertical rotation (90˚ FOV)
+    // has been optimized/factorized from prevision, formula might seem odd
+    mouseScreenRatioX = 2 * event.clientY / window.innerHeight - 1;
+    camera.rotation.x = - mouseScreenRatioX * PI / 2;
 
 }
 
@@ -48,11 +131,11 @@ document.addEventListener("DOMContentLoaded", function() {
     SC.get('/tracks', {
         q: 'erased tapes'
     }).then(function(tracks){
-        console.log(tracks);
         // // SC.stream(tracks[0].uri).then(function(player){
         // //     trackPlayer = player;
         // // });
-        // renderHome();
+        renderHome(tracks);
+        rotate();
         // STOCKER LES TRACKS et LOADER LA TRACK par HTTP lors de la lecture avec WEB AUDIO API
     });
 
