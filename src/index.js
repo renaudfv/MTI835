9f565cb55307b4974365e8c3ff3b8f3ef039e6d0
Renaud Vincent <renaudvincent@Renauds-MacBook-Pro.local>
2016-07-18 00:18:45 -0400

    Device orietation in progress

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ var _ = require('underscore');
 var SC = require('soundcloud');
 // from http://vr.chromeexperiments.com/
 var StereoEffect = require('../libs/threejs-vr/StereoEffect.js');
+var DeviceOrientationControls = require('../libs/threejs-vr/DeviceOrientationControls.js');
 THREE.StereoEffect = StereoEffect;
+THREE.DeviceOrientationControls = DeviceOrientationControls;
 
 var PI = Math.PI;
 
-var scene, camera, tracksGroup, trackPlayGroup, effect, cursor, source;
+var scene, camera, tracksGroup, trackPlayGroup, effect, cursor, source, controls;
 
 //WebGL rendering engine
 var renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -39,24 +41,26 @@ if(window.chrome) {
 
     recognition.onspeechend = function() {
       recognition.stop();
-    }
+  }
 
-    recognition.onresult = function(event) {
+  recognition.onresult = function(event) {
       var voiceResult = event.results[0][0].transcript;
       console.log(voiceResult);
       if(!!voiceResult)
         querySoundcloud(voiceResult);
-    }   
+}   
 }
 
 
 // Renders and update with browser refresh rate
 function render() {
     requestAnimationFrame( render );
+
     if(!toggleStereo)
         renderer.render( scene, camera );
     else 
         effect.render( scene, camera );
+
 }
 
 // Camera center cursor for VR context
@@ -149,6 +153,8 @@ function renderHome(tracks) {
 
     render();
     window.addEventListener("mousemove", onMouseMoveHome); 
+    window.addEventListener('deviceorientation', handleOrientation);
+
 } 
 
 function renderTrack(track) {
@@ -228,7 +234,7 @@ function renderTrack(track) {
     request.send();    
     window.addEventListener("mousemove", onMouseMoveTrack);
     if(toggleStereo) renderCursor();
-
+    window.addEventListener('deviceorientation', handleOrientation);
     
 }
 
@@ -384,7 +390,7 @@ window.addEventListener("click", function() {
 
 });
 
-document.getElementById('c-logo').addEventListener("click", function() {
+function toggleStereoF() {
     console.log('make it stereo');
     if(!toggleStereo) {
 
@@ -419,7 +425,9 @@ document.getElementById('c-logo').addEventListener("click", function() {
 
         render();
     }
-});
+}
+
+document.getElementById('c-logo').addEventListener("click", toggleStereoF);
 
 function requestFullscreen() {
     if (container.requestFullscreen) {
@@ -446,19 +454,34 @@ function exitFullscreen() {
 }
 
 function handleOrientation(event) {
-    var absolute = event.absolute;
-    var alpha    = event.alpha; // Z
-    var beta     = event.beta; // X
-    var gamma    = event.gamma; // Y
+    var absolute = THREE.Math.degToRad(event.absolute);
+    var alpha    = THREE.Math.degToRad(event.alpha); // Z
+    var beta     = THREE.Math.degToRad(event.beta); // X
+    var gamma    = THREE.Math.degToRad(event.gamma-90); // Y
 
-    if(toggleTrack) 
+    if(!!trackPlayGroup && toggleTrack) 
         trackPlayGroup.rotation = new THREE.Vector3(beta, gamma, alpha);
-    else    
-        tracksGroup.rotation = new THREE.Vector3(beta, gamma, alpha);
+    if(!!tracksGroup && ! toggleTrack)   {
+        tracksGroup.rotation.x  = -gamma;
+        tracksGroup.rotation.y = -alpha;
+        // tracksGroup.rotation.z = alpha;
+    } 
+
 
 }
 
-window.addEventListener('deviceorientation', handleOrientation);
+// function setOrientationControls(e) {
+//     if (!e.alpha) {
+//       return;
+//   }
+
+//   controls = new THREE.DeviceOrientationControls(camera, true);
+//   controls.connect();
+//   controls.update();
+
+//   window.removeEventListener('deviceorientation', setOrientationControls, true);
+// }
+
 
 function querySoundcloud(query) {
     //Ask for more tracks just in cas SC sends less
@@ -473,6 +496,21 @@ function querySoundcloud(query) {
 
     });
 }
+
+//http://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+}
+
+document.body.addEventListener("click", function() {
+
+    if(mobilecheck) {
+        toggleStereoF();
+    }
+    
+});
 
 // Will call every setup funcitno
 document.addEventListener("DOMContentLoaded", function() {
